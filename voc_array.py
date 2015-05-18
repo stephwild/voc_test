ff091d5e68dafb6b0abed1c57be2f7e27d7c3418
DELGADO Stephane <stephane.delgado@epita.fr>
2015-05-18 19:50:53 +0200

    Add block handler and VocItem object

--- voc_array.py
+++ voc_array.py
@@ -6,20 +6,65 @@ delim = ' - '
 def fill_voc_dic(str):
     f = open(str, 'r')
 
-    for line in f:
-        item = line_handler(line)
+    str_item = ""
+    item = None
 
-        if item is not None:
-            l.append(item)
+    for line in f:
+
+        if line[0] == ' ' or line[0] == '\t':
+            str_item += line
+        else:
+            item = block_handler(str_item)
+
+            if item is not None:
+                l.append(item)
+
+            str_item = line
+
+
+def block_handler(str_item):
+    if str_item == "":
+        return
+
+    tab_item = str_item.split('\tDesc: ', 1)
+
+    if len(tab_item) > 1:
+        tmp = tab_item.pop()  # Contain 'Desc' and 'Ex' part
+
+        for i in tmp.split('\tEx: ', 1):  # list[0]: Desc, list[1]: Ex
+            tab_item.append(i)
+
+    # Now tab_item[0] = main_words, tab_item[1]: Desc, tab_item[2]: Ex
+    item = line_handler(tab_item[0])
+
+    if len(tab_item) > 1:
+        item = desc_handler(tab_item[1])
+
+    if len(tab_item) > 2:
+        item = ex_handler(tab_item[2])
+
+    return item
+
 
 def line_handler(line):
     l = line.split(delim)
     item = None
 
     if len(l) != 1:
-        item = VocItem(l[0], l[1], None, None)
+        item = VocItem(l[0], l[1])
 
     return item
+
+
+def desc_handler(item, desc):
+    item.set_description(desc.split('\tDesc: ', 1)[1])
+    return item
+
+
+def ex_handler(item, examples):
+    item.set_example(examples.split('\tEx: ', 1)[1])
+    return item
+
 
 def init_array(need_log):
     fill_voc_dic("vocabulary")
---
+++ vocitem.py
@@ -0,0 +1,13 @@
+class VocItem:
+    def __init__(self, en_name, fr_name):
+        self.en_name = en_name
+        self.fr_name = fr_name
+
+    def set_description(self, description):
+        self.description = description
+
+    def set_example(self, example):
+        self.example = example
+
+def pretty_print_vocitem(is_needed):
+    pass
