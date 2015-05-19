01b6078ae6a25c939ff1dc1fbf0dd8802f103d59
DELGADO Stephane <stephane.delgado@epita.fr>
2015-05-19 18:55:02 +0200

    Add pretty_print_entity function; correct pretty_print_vocitem function; change def_list to empty list (not None)

--- vocitem.py
+++ vocitem.py
@@ -1,36 +1,56 @@
 class VocEntity:
-    def __init__(self, name: str, genre: str, comment: str):
+    def __init__(self, name, genre, comment):
         self.name = name
         self.genre = genre
         self.comment = comment
 
 
 class VocItem:
-    def_list = None
+    def_list = []
     description = None
     example = None
 
-    def __init__(self, entity: VocEntity):
-        self.word = entity
+    def __init__(self, entity):
+        self.keyword = entity
 
-    def add_translation(self, name: str, genre: str, comment: str):
+    def add_translation(self, name, genre, comment):
         voc_entity = VocEntity(name, genre, comment)
         self.def_list.append(voc_entity)
 
-    def add_translation_entity(self, entity: VocEntity):
+    def add_translation_entity(self, entity):
         self.def_list.append(entity)
 
-    def set_description(self, description: str):
+    def set_description(self, description):
         self.description = description
 
-    def set_example(self, example: str):
+    def set_example(self, example):
         self.example = example
 
+def pretty_print_entity(entity):
+    print("Entity -> n: ", entity.name, ",g: ", entity.genre, ", c: ", entity.comment)
 
-def pretty_print_vocitem(l: list):
-    for i in range(len(l)):
-        print("En: " + l[i].word.name, end=" ---- ")
-        print("list: ")
+def pretty_print_vocitem(item):
 
-        for i in l[i].def_list:
-            print(i.name + ' - ')
+    if item is None:
+        print("VocItem is equal to None")
+        return
+
+    print("En: " + item.keyword.name)
+    print("list: ")
+
+    for i in item.def_list:
+        print(i.name + ' --- ')
+
+    if item.description is None:
+        desc = "None"
+    else:
+        desc = item.description
+
+    print("\nDesc: " + desc)
+
+    if item.example is None:
+        example = None
+    else:
+        example = item.example
+
+    print("\nEx: " + example)
