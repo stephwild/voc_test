e26aae7f51fe32b23abec0952ac57f559e5416e2
DELGADO Stephane <stephane.delgado@epita.fr>
2015-05-21 03:52:29 +0200

    Add basic voc test

--- voclearn_tui.py
+++ voclearn_tui.py
@@ -1,9 +1,88 @@
+import sys
+
 from random_draw import random_draw
 from voc_fill import init_array
 
 
-def simple_handler(i, l):
+def list_handler(i, l):
     print(l[i].keyword.name)
 
-l = init_array(True)
-random_draw(l, simple_handler, 4)
+
+def print_Entity(entity):
+
+    if entity.genre is None:
+
+        if entity.comment is None:
+            print(entity.name)
+        else:
+            print(entity.name, '(' + entity.comment + ')')
+
+    else:
+        print(entity.name, entity.genre, '(' + entity.comment + ')')
+
+
+def print_VocItem(item):
+    print("------- CORRECTION -------")
+    print("\nKeyword: ", end="")
+    print_Entity(item.keyword)
+
+    print("def_list:")
+
+    for entity in item.def_list:
+        print("\t- ", end="")
+        print_Entity(entity)
+
+    if item.description is not None:
+        print("Description:", item.description)
+
+    if item.example is not None:
+        print("Example:", item.example)
+    print("\n----- END CORRECTION -----")
+
+
+# index of the VocItem, list of VocItem
+def traduc_handler(i, l):
+    retry = 0
+    succeed_test = False
+
+    while (retry < max_retry):
+        print("\nKeyword:", l[i].keyword.name)
+        print("Traduction: ", end="")
+        sys.stdout.flush()
+        response = sys.stdin.readline().strip(' \t\n')
+
+        for definion_entity in l[i].def_list:
+
+            if definion_entity.name == response:
+                succeed_test = True
+                break
+
+        if succeed_test:
+            global nbr_succeed
+            nbr_succeed = nbr_succeed + 1
+            break
+
+        retry = retry + 1
+
+    if succeed_test:
+        print("\nWell Played Dude, you succeed this one")
+    else:
+        print("\nFailed !")
+        print("More luck next time ! Check the response beneath:")
+        print_VocItem(l[i])
+        print("")
+
+
+nbr_succeed = 0
+nbr_test = 4
+max_retry = 3
+
+# Fill the VocItem array with the specified file
+l = init_array(False)
+
+# Run the tests
+print('Vocabulary test will begin !')
+print('Today, there will has', str(nbr_test), 'tests')
+random_draw(l, traduc_handler, nbr_test)
+
+print("\nYour score is", str(nbr_succeed) + "/" + str(nbr_test))
