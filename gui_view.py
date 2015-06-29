1b2d07cb4efc6b58b54d3878aed9f22722e3cd74
DELGADO Stephane <stephane.delgado@epita.fr>
2015-06-29 14:40:43 +0200

    First part kivy finish: Now Quizz part work !

--- gui_view.py
+++ gui_view.py
@@ -1,10 +1,10 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import StringProperty, NumericProperty
+from kivy.properties import StringProperty, NumericProperty, ObjectProperty
 
 from voc_fill import init_array
-#from random_draw import random_draw
+from random_draw import random_draw
 
 nbr_succeed = 0
 nbr_test = 4
@@ -16,19 +16,58 @@ Builder.load_file('kv/quizz.kv')
 #Builder.load_file('kv/settings.kv')
 
 class MenuScreen(Screen):
-    def run_quizz():
-        pass
+    def run_quizz(self):
+        sm.current = 'quizz'
 
-    def run_voclist():
-        pass
+    def run_voclist(self):
+        sm.current = 'voclist'
 
-    def run_settings():
-        pass
+    def run_settings(self):
+        sm.current = 'settings'
 
 class QuizzScreen(Screen):
     max_retries = NumericProperty(3)
     keyword = StringProperty('Hello World')
-    pass
+    rsp_input = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(QuizzScreen, self).__init__(**kwargs)
+
+        # Fill the VocItem array with the specified file
+        tab = init_array(False)
+        self.random_tab = random_draw(tab, nbr_test)
+        self.i = 0
+
+        self.init_quizz_entry()
+
+    def init_quizz_entry(self):
+        self.retry = 0
+        self.current_vocitem = self.random_tab[self.i]
+        self.keyword = self.current_vocitem.keyword.to_String()
+
+    def on_submit(self, instance):
+        next_test = False
+
+        if self.current_vocitem.is_good_translation(self.rsp_input.text):
+            # Say that you win
+            next_test = True
+        else:
+            if self.retry < self.max_retries:
+                self.retry = self.retry + 1
+            else:
+                # Say that you fail
+                # Here you have to lauch a new screen with the solution
+                next_test = True
+
+        if next_test:
+            self.i = self.i + 1
+            self.rsp_input.text = ""
+
+            if self.i < len(self.random_tab):
+                self.init_quizz_entry()
+            else:
+                sm.current = 'menu'
+
 
 class VocListScreen(Screen):
     pass
@@ -44,10 +83,6 @@ sm.add_widget(SettingsScreen(name='settings'))
 
 class VocApp(App):
     def build(self):
-        # Fill the VocItem array with the specified file
-        tab = init_array(False)
-        sm.current = 'quizz'
-
         return sm
 
 if __name__ == '__main__':
--- vocitem.py
+++ vocitem.py
@@ -4,6 +4,16 @@ class VocEntity:
         self.genre = genre
         self.comment = comment
 
+    def to_String(self):
+        if self.genre is None and self.comment is None:
+            return self.name
+        elif self.genre is None:
+            return self.name + " (" + self.comment + ")"
+        elif self.comment is None:
+            return self.name + " [" + self.genre + "]"
+
+        return self.name + " [" + self.genre + "] " + "(" + self.comment + ")"
+
 
 class VocItem:
     def __init__(self, entity):
@@ -24,3 +34,22 @@ class VocItem:
 
     def set_example(self, example):
         self.example = example
+
+    def get_keyword(self):
+        return self.keyword.to_String()
+
+    def get_description(self):
+        return self.description
+
+    def get_example(self):
+        return self.example
+
+    def nbr_traduction(self):
+        return len(self.def_list)
+
+    def is_good_translation(self, response):
+        for definion_entity in self.def_list:
+            if definion_entity.name == response:
+                return True
+
+        return False
